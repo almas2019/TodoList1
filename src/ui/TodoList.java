be5298d7fa22f97e8f88ceaa2adcdee0b9b57e29
Almas <m4o0b@ugrad.cs.ubc.ca>
2018-09-10 10:03:36 -0700

    Added a main method and 2 additional methods it calls

---
+++ src/ui/TodoList.java
@@ -0,0 +1,18 @@
+package ui; //name of package//
+
+public class TodoList {
+
+    public static void main(String[] args) {
+        title();
+        leftmssg();
+    }
+
+
+    public static void title() {
+        System.out.println("My To-Dos");
+    } // Display the string.
+
+    public static void leftmssg() {
+    System.out.println("You have some tasks left");}
+}
+
