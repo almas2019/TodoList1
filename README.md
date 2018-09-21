386decbf09d18861bb5ac0fe16455552b9da4500
Almas <m4o0b@ugrad.cs.ubc.ca>
2018-09-20 23:08:07 -0700

    Added a while loop and created basic list functionality, added a DailyList class

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 [1]: https://edge.edx.org/courses/course-v1:UBC+CPSC210+2018W1/courseware/a4d49b3ef5fa4fe2bd9496e76d72dc48/e2887456a15a48dbb040ecdac313168f/1?activate_block_id=block-v1%3AUBC%2BCPSC210%2B2018W1%2Btype%40vertical%2Bblock%40ff793bbcd5544e82bb5052f0dffe5d71
 
 Created a main method with 2 additional methods the main method calls
+
+//For Sept 21:
+Added a DailyList class
+created a while loop containing 4 basic functions: adding an item to a not done list, moving an item from a not done to a done list, printing out all items in the list
+also creating a method to quit the loop
+Code adopted from logging calculator
--- src/ui/TodoList.java
+++ src/ui/TodoList.java
@@ -1,18 +1,77 @@
 package ui; //name of package//
 
-public class TodoList {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        title();
-        leftmssg();
+
+public class TodoList {
+    Scanner scanner = new Scanner(System.in);
+    DailyList list = new DailyList();
+
+    public TodoList() {
+        String option = "";
+
+
+        while (true) {
+
+            System.out.println("Please select an option: [1] Add an item  [2] Cross off an item [3] show all items");
+            option = scanner.nextLine();
+            System.out.println("you selected: " + option);
+            if (option.equals("1")) {
+                enter(list);
+                System.out.println("The number of tasks left to do:");
+                System.out.println(list.listentries.size());
+            }
+            if (option.equals("2")) {
+                remove(list);
+                System.out.println("The Number of things that are done");
+                System.out.println(list.crossofflist.size());
+            }
+            if (option.equals("3")) {
+                printItems(list);
+                int lengths = listSize(list.listentries, list.crossofflist);
+                System.out.println(lengths);
+            } else if (option.equals("quit")) {
+                System.out.println("This application will now shut down");
+                break;
+            }
+
+
+        }
+    }
+
+
+    public void enter(DailyList list) {
+        System.out.println("Enter the item text");
+        String addition = scanner.nextLine();
+        list.addtoList(addition);
 
     }
 
-    public static void title() {
-        System.out.println("My To-Dos");
-    } // Display the string.
 
-    public static void leftmssg() {
-    System.out.println("You have some tasks left today");}
+    public void remove(DailyList list) {
+        System.out.println("What task are you done?");
+        list.print(list.listentries);
+        String checkoff = scanner.nextLine();
+        list.takeoutEntries(checkoff);
+    }
+
+
+    public void printItems(DailyList list) {
+        list.print(list.listentries);
+    }
+
+    public int listSize(ArrayList<String> list1, ArrayList<String> list2) {
+        System.out.println("These are the total number of tasks from both lists");
+        int a = list1.size();
+        int b = list2.size();
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        new TodoList();
+    }
+
+
 }
-
+//code reference from Logging Calculator
