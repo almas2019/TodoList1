29e94d8ffa17564f6c351ad7d61d95bf83196613
Almas <m4o0b@ugrad.cs.ubc.ca>
2019-01-31 19:25:09 -0800

    edited save method a bit

--- src/ui/Loadable.java
+++ src/ui/Loadable.java
@@ -6,5 +6,5 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 
 public interface Loadable {
-    public void loadArray(String s, EntryManager em) throws IOException, JSONException;
+    void loadArray(String s, EntryManager em) throws IOException, JSONException;
 }
